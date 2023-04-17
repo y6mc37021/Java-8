889107347214ec8a1a8823d383f02a592855f7cc
Krishna Karthik <tkarthik21@gmail.com>
2023-04-17 19:37:47 +0100

    Lambda Expressions Exercises

--- Functional Programming/src/functional/programming/lambda/expression/exercises/Exercise_1_Java_8_1.java
+++ Functional Programming/src/functional/programming/lambda/expression/exercises/Exercise_1_Java_8_1.java
@@ -2,6 +2,7 @@ package functional.programming.lambda.expression.exercises;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import functional.programming.lambda.expression.Person;
@@ -12,39 +13,41 @@ import functional.programming.lambda.expression.Person;
  * - Print the list
  * - Print objects that have last name beginning with 'C'
  * */
-public class Exercise_1_Java_8 {
+public class Exercise_1_Java_8_1 {
 
 	public static void main(String[] args) {
 		
 		List<Person> person = Arrays.asList(
-				new Person("Petter", "Coach", 45),
 				new Person("Ravi", "Kumar", 45),
 				new Person("John", "Perk", 45),
 				new Person("Oliver", "Scale", 45),
 				new Person("Alan", "Bukka", 45),
 				new Person("Paul", "Castle", 45)
 				);
-		System.out.println("Print All Persions");
-		printOnCondition(person, p->true);
-		System.out.println("Print Sorted List");
+		
 		Collections.sort(person, (o1,o2)->o1.getLastName().compareTo(o2.getLastName()));
-		printOnCondition(person, p->true);
-		System.out.println("Print Persons Whose Last Names Starts With C ");
-		printOnCondition(person, p->p.getLastName().startsWith("C"));
 		
+		printConditionally con = perObj -> {
+			for(Person p : person) {
+				if(p.getLastName().startsWith("C"))
+					System.out.println(p);
+			}
+		};
+		con.printListLastNameStartsWithC(person);
 		
+		printConditionally printAll = perObj -> {
+			System.out.println("------");
+			for(Person p : person) {
+					System.out.println(p);
+			}
+		};
+		printAll.printListLastNameStartsWithC(person);
 	}		
 	
-	public static void printOnCondition(List<Person> person, Condition condition) {
-		
-		for(Person p : person) {
-			if(condition.test(p)) {
-				System.out.println(p);
-			}	
-		}
-	}
+	
 }
 
-interface Condition{
-	public boolean test(Person p);
+interface printConditionally{
+	public  void printListLastNameStartsWithC(List<Person> person);
 }
+
