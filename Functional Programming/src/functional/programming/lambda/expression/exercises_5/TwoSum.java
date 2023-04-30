9bebe669fe07fe2fd8192a73fba514a93d2294c2
y6mc37021 <tkarthik21@gmail.com>
2023-05-01 00:18:19 +0100

    Lambda Expression Drill

--- Functional Programming/src/functional/programming/lambda/expression/exercises_5/TwoSum.java
+++ Functional Programming/src/functional/programming/lambda/expression/exercises_5/TwoSum.java
@@ -1,13 +1,16 @@
 package functional.programming.lambda.expression.exercises_5;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
 	public static void main(String[] args) {
-		int[] arr = {3,3};
-		int target = 6;
-		solution(arr,target);
+		int[] arr = {3,7,4,1,2,};
+		int target = 9;
+		//solution(arr,target);
+		betterSolution(arr,target);
 	}
 	
 	public static void solution(int[] arr, int target ) {
@@ -26,6 +29,23 @@ public class TwoSum {
 		System.out.println("Output Length = "+tarr.length);
 		System.out.println("Output = "+Arrays.toString(tarr));
 		
+	}
+	
+	public static int[] betterSolution(int[] nums, int target) {
+	        int n=nums.length;
+	        Map<Integer,Integer> map=new HashMap<>();
+	        int[] result=new int[2];
+	        for(int i=0;i<n;i++){
+	            if(map.containsKey(target-nums[i])){
+	                result[1]=i;
+	                result[0]=map.get(target-nums[i]);
+	                return result;
+	            }
+	            map.put(nums[i],i);
+	        }
+	        
+	        return result;
+
 	}
 	
 	
