5342d5b5e1e36e80f0c61517115b93399f0459db
y6mc37021 <tkarthik21@gmail.com>
2023-05-08 21:56:45 +0100

    Binary Search Recursive

--- Functional Programming/src/com/interview/preparation/BinarySearch.java
+++ Functional Programming/src/com/interview/preparation/BinarySearch.java
@@ -4,12 +4,18 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 		int[] arry = {1,2,3,4,6,7,8,10};
-		int key=9;
-		binarySearch(arry, key);
+		int key=8;
+		int low=0, high=arry.length-1;
+		//binarySearch(arry, key, low, high);
+		int result = binarySearch_non_recursive(arry, key, low, high);
+		//int result = binarySearch_recursive(arry, key, low, high);
+		if (result == -1)  
+            System.out.println("\nKey not found in given list!");  
+        else 
+            System.out.println("\nKey is found at location: "+result + " in the list"+" Element = "+arry[6]); 
 	}
 	
-	public static void binarySearch(int[] arr, int target) {
-		int low=0,high=arr.length-1;
+	public static void binarySearch(int[] arr, int target, int low, int high) {
 		boolean b=false;
 		while(low<=high) {
 			int middle=(low+high)/2;
@@ -28,7 +34,47 @@ public class BinarySearch {
 		if(!b) {
 			System.out.println("Element Not Found");
 		}
-		 
+	}
+	public static int binarySearch_recursive(int[] arr, int target, int low, int high) {
 		
+		int middle=(low+high)/2;
+		int middlenumber=arr[middle];
+		if(low<=high){
+			if(middlenumber==target) {
+				System.out.println("Element Found");
+				return middle;
+			}	
+			if(middlenumber<target) {
+				low = middle+1;
+				return binarySearch_recursive(arr,target,low,high);
+			}else {
+				high = middle-1;
+				return binarySearch_recursive(arr,target,low,high);
+			}
+		}
+		return -1;
+	}
+	
+	
+	public static int binarySearch_non_recursive(int[] arr, int target, int low, int high) {
+		
+		int middle=(low+high)/2;
+		int middlenumber=arr[middle];
+		while(low<=high){
+			if(middlenumber==target) {
+				System.out.println("Element Found");
+				return middle;
+			}	
+			if(middlenumber<target) {
+				low = middle+1;
+				//return binarySearch_recursive(arr,target,low,high);
+			}else {
+				high = middle-1;
+				//return binarySearch_recursive(arr,target,low,high);
+			}
+			middle=(low+high)/2;
+			middlenumber=arr[middle];
+		}
+		return -1;
 	}
 }
