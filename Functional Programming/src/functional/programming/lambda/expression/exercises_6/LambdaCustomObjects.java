30f488f6c4c98d924639c5aa9ec2c2d3121754e3
y6mc37021 <tkarthik21@gmail.com>
2023-05-12 23:46:05 +0100

    Lambda Custom Object Sort

---
+++ Functional Programming/src/functional/programming/lambda/expression/exercises_6/LambdaCustomObjects.java
@@ -0,0 +1,29 @@
+package functional.programming.lambda.expression.exercises_6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LambdaCustomObjects {
+	
+	public static void main(String[] args) {
+		List<Employee> emp = new ArrayList<Employee>();
+		emp.add(new Employee("Security","Brut", 900L));
+		emp.add(new Employee("SE","Amy", 100L));
+		emp.add(new Employee("HR","Dean", 500L));
+		emp.add(new Employee("SE","Oliv", 700L));
+		emp.add(new Employee("Accts","Peter", 300l));
+		emp.add(new Employee("HR","Jany", 200L));
+		
+		Collections.sort(emp, (o1,o2)->o2.getName().compareToIgnoreCase(o1.getName()));
+		System.out.println(emp);
+		Collections.sort(emp, (o1,o2)->o1.getName().compareToIgnoreCase(o2.getName()));
+		System.out.println(emp);
+		Collections.sort(emp, (o1,o2)->(o1.getId()>o2.getId())?1:(o1.getId()<o2.getId()?-1:0));
+		System.out.println(emp);
+		Collections.sort(emp, (o1,o2)->(o1.getId()>o2.getId())?-1:(o1.getId()<o2.getId()?1:0));
+		System.out.println(emp);
+		
+	}
+
+}
