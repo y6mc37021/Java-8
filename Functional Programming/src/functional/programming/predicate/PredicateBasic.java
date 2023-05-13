427e97e6adf9b41d0afdc8ff61af1a23649192a5
y6mc37021 <tkarthik21@gmail.com>
2023-05-13 10:43:47 +0100

    Lambda & Predicate Basics

---
+++ Functional Programming/src/functional/programming/predicate/PredicateBasic.java
@@ -0,0 +1,27 @@
+package functional.programming.predicate;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateBasic {
+
+	public static void main(String[] args) {
+		
+		Predicate<Integer> p1 = I->I>10;
+		System.out.println(p1.test(20));
+		
+		Predicate<String> p2 = S->S.contentEquals("Hello");
+		System.out.println(p2.test("He"));
+		
+		Predicate<String> p3 = S->S.length()>3;
+		System.out.println(p3.test("Hello"));
+		
+		Predicate<Collection<String>> p4 = P->P.isEmpty();
+		List<String> aa1= List.of("AA");
+		System.out.println(p4.test(aa1));
+		List<String> aa2= List.of();
+		System.out.println(p4.test(aa2));
+	}
+	
+}
